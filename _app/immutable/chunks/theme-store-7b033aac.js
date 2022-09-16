bf92fbd98f83c7fcec888372a4a7c953c24a57b4
Jonas Hoersch <jonas.hoersch@climateanalytics.org>
2022-09-16 12:11:25 +0200

    initial commit

---
+++ _app/immutable/chunks/theme-store-7b033aac.js
@@ -0,0 +1,1 @@
+import{w as r}from"./index-57b0d6bc.js";var o=r();export{o as T};
---
+++ _app/immutable/pages/explore/index.svelte-c8c56c0b.js
@@ -0,0 +1,7 @@
+import{S as f,i as g,s as v,k as x,e as c,t as q,T as E,d as i,m as b,c as m,a as u,h as L,b as y,g as p,P as h,E as n}from"../../chunks/index-08858dcd.js";function k(_){let a,t,o,s;return{c(){a=x(),t=c("div"),o=c("p"),s=q(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua.`),this.h()},l(e){E('[data-svelte="svelte-emkch2"]',document.head).forEach(i),a=b(e),t=m(e,"DIV",{class:!0});var r=u(t);o=m(r,"P",{});var l=u(o);s=L(l,`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua.`),l.forEach(i),r.forEach(i),this.h()},h(){document.title="Explore",y(t,"class","explore-intro")},m(e,d){p(e,a,d),p(e,t,d),h(t,o),h(o,s)},p:n,i:n,o:n,d(e){e&&i(a),e&&i(t)}}}class S extends f{constructor(a){super(),g(this,a,null,k,v,{})}}export{S as default};
